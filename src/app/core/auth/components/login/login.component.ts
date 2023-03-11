db3c3b31d080e48e7371a6df790c00717ec75b22
Fernando M. Yncaluque <fyncaluque@gmail.com>
2023-03-10 22:16:05 -0500

    Binding form login

--- src/app/core/auth/components/login/login.component.ts
+++ src/app/core/auth/components/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    // Inicializado el formulario y validaciones con formbuilder
+    this.form = this.fb.group({
+      email: [, [Validators.required, Validators.email]],
+      password: [, [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
