c55f3e749fc65f975539390be0c9fe9efe22bef0
Fernando M. Yncaluque <fyncaluque@gmail.com>
2023-03-11 19:18:32 -0500

    Add firebase in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-pos-d90a9',
+    appId: '1:508075001725:web:d3230da52fa0f835b46e14',
+    storageBucket: 'angular-pos-d90a9.appspot.com',
+    apiKey: 'AIzaSyAU55lDITJwowK5hASqhLTMmNdho0_alhY',
+    authDomain: 'angular-pos-d90a9.firebaseapp.com',
+    messagingSenderId: '508075001725',
+  },
+  production: false,
+};
